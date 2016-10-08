13a27ba5ee2ab93e0efd4be414cf107105209a1b
Ryan Robertson <rjr.devry@gmail.com>
2016-10-08 09:13:06 -0700

    Implementation of permutation validation coding interview practice problem.

---
+++ Permutation Validation/PermutationValidator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace Permutation_Validation
+{
+    public static class PermutationValidator
+    {
+        public static bool IsValidPermutation(string wordOne, string wordTwo)
+        {
+            Dictionary<char, int> mapOne = BuildMap(wordOne);
+            Dictionary<char, int> mapTwo = BuildMap(wordTwo);
+            return AreEqual(mapOne, mapTwo);
+        }
+
+        private static Dictionary<char, int> BuildMap(string word)
+        {
+            var map = new Dictionary<char, int>();
+
+            // Guard against null or empty strings.
+            if (string.IsNullOrEmpty(word)) return map;
+
+            // Loop through each character in the string.
+            foreach (var c in word.ToCharArray())
+            {
+                // If the character is already in the dictionary (hashmap), increment its count.
+                int val;
+                if (map.TryGetValue(c, out val))
+                {
+                    map[c] = val + 1;
+                }
+                // Otherwise, record the first occurence of this character.
+                else map.Add(c, 1);
+            }
+
+            return map;
+        }
+
+        private static bool AreEqual(Dictionary<char, int> mapOne, Dictionary<char, int> mapTwo)
+        {
+            // If the words contain a different set of characters, they are not permutations.
+            if (mapOne.Count != mapTwo.Count) return false;
+
+            foreach (var keyValuePair in mapOne)
+            {
+                // If the character is not in both hashmaps, the words are not permutations.
+                if (!mapTwo.ContainsKey(keyValuePair.Key)) return false;
+
+                // If the character occurs a different number of times, the words are not permutations.
+                if (keyValuePair.Value != mapTwo[keyValuePair.Key]) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Permutation Validation/PermutationValidatorTest.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+
+namespace Permutation_Validation
+{
+    [TestFixture]
+    class PermutationValidatorTest
+    {
+        [Test]
+        public void CatActTest()
+        {
+            string wordOne = "cat";
+            string wordTwo = "act";
+            bool isPermuation = PermutationValidator.IsValidPermutation(wordOne, wordTwo);
+            Assert.IsTrue(isPermuation);
+        }
+
+        [Test]
+        public void CatCatTest()
+        {
+            string wordOne = "cat";
+            string wordTwo = "cat";
+            bool isPermuation = PermutationValidator.IsValidPermutation(wordOne, wordTwo);
+            Assert.IsTrue(isPermuation);
+        }
+
+        [Test]
+        public void CatHatTest()
+        {
+            string wordOne = "cat";
+            string wordTwo = "hat";
+            bool isPermuation = PermutationValidator.IsValidPermutation(wordOne, wordTwo);
+            Assert.IsFalse(isPermuation);
+        }
+
+        [Test]
+        public void CatEmptyStringTest()
+        {
+            string wordOne = "cat";
+            string wordTwo = "";
+            bool isPermuation = PermutationValidator.IsValidPermutation(wordOne, wordTwo);
+            Assert.IsFalse(isPermuation);
+        }
+
+        [Test]
+        public void CatNullTest()
+        {
+            string wordOne = "cat";
+            string wordTwo = null;
+            bool isPermuation = PermutationValidator.IsValidPermutation(wordOne, wordTwo);
+            Assert.IsFalse(isPermuation);
+        }
+
+        // Code won't compile if one of the arguments is not initialized.  A ReSharper protection?
+
+        //[Test]
+        //public void CatNotInitializedTest()
+        //{
+        //    string wordOne = "cat";
+        //    string wordTwo;
+        //    bool isPermuation = PermutationValidator.IsValidPermutation(wordOne, wordTwo);
+        //    Assert.IsFalse(isPermuation);
+        //}
+
+        [Test]
+        public void EmptyStringNullTest()
+        {
+            string wordOne = "";
+            string wordTwo = null;
+            bool isPermuation = PermutationValidator.IsValidPermutation(wordOne, wordTwo);
+            Assert.IsTrue(isPermuation);
+        }
+
+        // Code won't compile if one of the arguments is not initialized.  A ReSharper protection?
+
+        //[Test]
+        //public void EmptyStringNotInitializedTest()
+        //{
+        //    string wordOne = "";
+        //    string wordTwo;
+        //    bool isPermuation = PermutationValidator.IsValidPermutation(wordOne, wordTwo);
+        //    Assert.IsTrue(isPermuation);
+        //}
+    }
+}
